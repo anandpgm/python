cf2941358bfdd2d637fd0869e9fc0ce7dce8b893
anandk <anandk@hp.com>
2019-12-11 23:47:42 +0530

    new pgm

---
+++ books/taming/introduction/print.py
@@ -0,0 +1,11 @@
+'''
+Program that accept an integer (n) and computes the value of n+nn+nnn
+'''
+
+num = input('Enter the number: ')
+
+n1 = int(num)
+n2 = int("%s%s" % (num,num))
+n3 = int("%s%s%s" % (num,num,num))
+print(n1, "", n2, "", n3)
+print("Sum is", (n1+n2+n3))
